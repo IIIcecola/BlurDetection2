87b2a088ad81984d76bc4290ec0be4bd0e3a210a
IIIcecola <119150034+IIIcecola@users.noreply.github.com>
2025-10-24 14:21:52 +0800

    Import BlurDetect from blur_detection module

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import cv2
 import numpy as np
 
 from blur_detection.detection import estimate_blur, fix_image_size, pretty_blur_map
+from blur_detection.blur_detection import BlurDetect
 
 # 配置日志
 logging.basicConfig(
